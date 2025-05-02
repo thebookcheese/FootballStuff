b02a876e18446d5f62bd05704bdd4aebfde76c56
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2025-05-02 07:26:30 +0000

    Added Relative Strengths

--- analyse.py
+++ analyse.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 DataFolder = 'Data/'
-Years = ['1950-51/', '1951-52/','1952-53/', '1953-54/']
+Years = ['1950-51/', '1951-52/','1952-53/', '1953-54/', '1954-55/', '1955-56/','1956-57/']
 Data = {
     '1950-51/': [
         'first_division_results.csv',
@@ -26,7 +26,25 @@ Data = {
         'second_division_results.csv',
         'third_division_north_results.csv',
         'third_division_south_results.csv'
-    ]
+    ],
+    '1954-55/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_north_results.csv',
+        'third_division_south_results.csv'
+    ],
+    '1955-56/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_north_results.csv',
+        'third_division_south_results.csv'
+    ],
+    '1956-57/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_north_results.csv',
+        'third_division_south_results.csv'
+    ],
 }
 
 
@@ -46,6 +64,7 @@ def flat(lis):
 pd.options.display.max_rows = 9999
 wincounter = {}
 scorecounter = {}
+relativestrengths = {}
 FullResults = []
 for year in Years:
     for data in Data[year]:
@@ -63,6 +82,34 @@ for year in Years:
                 AwayResults[i] = str(AwayResults[i])[::-1]
             Results = [HomeResults, AwayResults]
             FullResults.append(Results)
+            c = flat(Results)
+            if type(c) == list:
+                c = flat(c)
+            name = c[0]
+            count = 0
+            for i in c:
+                if count == 0:
+                    pass
+                else:
+                    if type(i) == float:
+                        pass
+                    else:
+                        if i[0] > i[2]:
+                            if name not in relativestrengths:
+                                if data == 'first_division_results.csv':
+                                    relativestrengths[name] = 1
+                                elif data == 'second_division_results.csv':
+                                    relativestrengths[name] = 0.5
+                                elif data == 'third_division_north_results.csv' or data == 'third_division_south_results.csv':
+                                    relativestrengths[name] = 0.25
+                            else:
+                                if data == 'first_division_results.csv':
+                                    relativestrengths[name] = relativestrengths[name] + 1
+                                elif data == 'second_division_results.csv':
+                                    relativestrengths[name] = relativestrengths[name] + 0.5
+                                elif data == 'third_division_north_results.csv' or data == 'third_division_south_results.csv':
+                                    relativestrengths[name] = relativestrengths[name] + 0.25
+                count = count + 1
 with open('writeto.txt', 'w') as f:
     for i in FullResults:
         b = flat(i)
@@ -74,7 +121,7 @@ with open('writeto.txt', 'w') as f:
                 if type(b[c]) != float:
                     if b[c][0] > b[c][2]:
                         if name not in wincounter:
-                            wincounter[name] = 2
+                            wincounter[name] = 2 
                         else:
                             wincounter[name] = wincounter[name] + 1
                     if b[c] not in scorecounter:
@@ -83,11 +130,17 @@ with open('writeto.txt', 'w') as f:
                         scorecounter[b[c]] = scorecounter[b[c]] + 1
     sortedwincounter = dict(sorted(wincounter.items(), key=lambda x: x[1], reverse=True))
     sortedscorecounter = dict(sorted(scorecounter.items(), key=lambda x: x[1], reverse=True))
+    sortedrelativestrengths = dict(sorted(relativestrengths.items(), key=lambda x: x[1], reverse=True))
     f.write('Club Wins \n')
     count = 0
     for d in sortedwincounter:
             f.write(str(d) + ' won ' + str(wincounter[d]) + ' matches \n')
     f.write('\n')
+    f.write('Relative Strength \n')
+    for d in sortedrelativestrengths:
+            f.write(str(d) + ' had a relative strength of ' + str(relativestrengths[d]) + ' \n')
+    f.write('\n')
+    f.write('Scores \n')
     for d in sortedscorecounter:
         if count % 2 == 0:
             f.write(str(d) + ' happend ' + str(scorecounter[d]) + ' times \n')
