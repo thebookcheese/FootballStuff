2801f3b45c58dad1a05cec914723b19db637d6c3
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2025-05-02 10:06:32 +0000

    More football data

--- analyse.py
+++ analyse.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 DataFolder = 'Data/'
-Years = ['1950-51/', '1951-52/','1952-53/', '1953-54/', '1954-55/', '1955-56/','1956-57/']
+Years = ['1950-51/', '1951-52/','1952-53/', '1953-54/', '1954-55/', '1955-56/','1956-57/', '1957-58/', '1958-59/', '1959-60/', '1960-61/', '1961-62/', '1962-63/', '1963-64/', '1964-65/', '1965-66/', '1966-67/', '1967-68/', '1968-69/', '1969-70/']
 Data = {
     '1950-51/': [
         'first_division_results.csv',
@@ -45,6 +45,84 @@ Data = {
         'third_division_north_results.csv',
         'third_division_south_results.csv'
     ],
+    '1957-58/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_north_results.csv',
+        'third_division_south_results.csv'
+    ],
+    '1958-59/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1959-60/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1960-61/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1961-62/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1962-63/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+     '1963-64/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1964-65/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1965-66/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1966-67/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+    '1967-68/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+     '1968-69/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
+      '1969-70/': [
+        'first_division_results.csv',
+        'second_division_results.csv',
+        'third_division_results.csv',
+        'fourth_division_results.csv'
+    ],
 }
 
 
@@ -65,6 +143,7 @@ pd.options.display.max_rows = 9999
 wincounter = {}
 scorecounter = {}
 relativestrengths = {}
+gamesplayed = {}
 FullResults = []
 for year in Years:
     for data in Data[year]:
@@ -79,7 +158,14 @@ for year in Years:
             HomeResults = list(a.iloc[i])
             AwayResults = list(a[Headers[i+1]])
             for i in range(len(AwayResults)):
-                AwayResults[i] = str(AwayResults[i])[::-1]
+                if type(AwayResults[i]) != float:
+                    split = AwayResults[i].split('–')
+                    split.insert(1, '-')
+                    z = split[0]
+                    y = split[2]
+                    split[0], split[2] = y, z
+                    for j in range(len(split)):
+                        AwayResults[i] = ''.join(split)
             Results = [HomeResults, AwayResults]
             FullResults.append(Results)
             c = flat(Results)
@@ -102,14 +188,23 @@ for year in Years:
                                     relativestrengths[name] = 0.5
                                 elif data == 'third_division_north_results.csv' or data == 'third_division_south_results.csv':
                                     relativestrengths[name] = 0.25
+                                elif data == 'fourth_division_results.csv':
+                                    relativestrengths[name] = 0.125
                             else:
                                 if data == 'first_division_results.csv':
                                     relativestrengths[name] = relativestrengths[name] + 1
                                 elif data == 'second_division_results.csv':
                                     relativestrengths[name] = relativestrengths[name] + 0.5
-                                elif data == 'third_division_north_results.csv' or data == 'third_division_south_results.csv':
+                                elif data == 'third_division_north_results.csv' or data == 'third_division_south_results.csv' or data == 'third_division_results.csv':
                                     relativestrengths[name] = relativestrengths[name] + 0.25
+                                elif data == 'fourth_division_results.csv': 
+                                    relativestrengths[name] = relativestrengths[name] + 0.125
                 count = count + 1
+            if name not in gamesplayed:
+                gamesplayed[name] = count
+            else:
+                gamesplayed[name] = gamesplayed[name] + count
+
 with open('writeto.txt', 'w') as f:
     for i in FullResults:
         b = flat(i)
@@ -130,7 +225,10 @@ with open('writeto.txt', 'w') as f:
                         scorecounter[b[c]] = scorecounter[b[c]] + 1
     sortedwincounter = dict(sorted(wincounter.items(), key=lambda x: x[1], reverse=True))
     sortedscorecounter = dict(sorted(scorecounter.items(), key=lambda x: x[1], reverse=True))
-    sortedrelativestrengths = dict(sorted(relativestrengths.items(), key=lambda x: x[1], reverse=True))
+    percentagerelativestrengths = {}
+    for key in relativestrengths:
+        percentagerelativestrengths[key] = ((relativestrengths[key] / gamesplayed[key]) * 100)
+    sortedrelativestrengths = dict(sorted(percentagerelativestrengths.items(), key=lambda x: x[1], reverse=True))
     f.write('Club Wins \n')
     count = 0
     for d in sortedwincounter:
@@ -138,7 +236,7 @@ with open('writeto.txt', 'w') as f:
     f.write('\n')
     f.write('Relative Strength \n')
     for d in sortedrelativestrengths:
-            f.write(str(d) + ' had a relative strength of ' + str(relativestrengths[d]) + ' \n')
+            f.write(str(d) + ' had a relative strength of ' + str(((relativestrengths[d] / gamesplayed[d]) * 100))[:5]+ '%  \n')
     f.write('\n')
     f.write('Scores \n')
     for d in sortedscorecounter:
